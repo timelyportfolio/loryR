93ab6aa69f47f90c86887cf5d3ddc252e533941e
timelyportfolio <kent.russell@timelyportfolio.com>
2015-05-14 13:49:26 -0500

    iterate off of pull by @kevinferris10 and improve rstudio_gallery by
    1. sorting files by time created
    2. only base64enc selected img

--- R/utils.R
+++ R/utils.R
@@ -12,14 +12,28 @@
 
 rstudio_gallery <- function(img = NULL){
   if(!(is.null(img) | is.numeric(img))) stop("img must be a numeric vector")
+
   images <- Filter(Negate(is.null),lapply(
     list.files(tempdir(),"png",recursive=T,full.names=T)
     ,function(p){
       if(grepl(x=p,pattern="rs-graphics") && !(grepl(x=p,pattern="empty"))){
-        base64enc::dataURI(file=p, mime="image/png")
-      }
+        return(p)
+      } else return(NULL)
     }
   ))
-  if(is.null(img)) images
-  else images[img]
+
+  # sort images by modified time
+  images <- images[order(
+                    sapply(images,function(i){file.info(i)[["mtime"]]})
+                  )]
+
+  # base64 encode images all if img == NULL or selected
+  lapply(
+    if(is.null(img)){
+      images
+    } else {
+      images[img]
+    }
+    ,function(p){ base64enc::dataURI(file=p, mime="image/png") }
+  )
 }
