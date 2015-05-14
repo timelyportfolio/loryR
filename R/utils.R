07ddd071aa36090cb9da07144f5347e86780308a
kferris10 <kevin.ferris10@gmail.com>
2015-05-14 10:51:38 -0600

    Add parameter to rstudio_gallery() which allows the user to select which images to store.

--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,18 @@
 #' Gallery of RStudio Plots
 #'
-#' rstudio_gallery() is a convenience function to grab all the plots from your RStudio history and
-#' return the images as a \code{list} of \code{base64} encoded images.
+#' rstudio_gallery() is a convenience function to grab all the plots from your
+#' RStudio history and return the images as a \code{list} of \code{base64}
+#' encoded images.
 #'
+#' @param img numeric vector containing indices for which plots to suggest.
+#'   Defaults to NULL, in which case all plots will be used.
 #' @import base64enc
 #'
 #' @export
 
-rstudio_gallery <- function(){
-  Filter(Negate(is.null),lapply(
+rstudio_gallery <- function(img = NULL){
+  if(!(is.null(img) | is.numeric(img))) stop("img must be a numeric vector")
+  images <- Filter(Negate(is.null),lapply(
     list.files(tempdir(),"png",recursive=T,full.names=T)
     ,function(p){
       if(grepl(x=p,pattern="rs-graphics") && !(grepl(x=p,pattern="empty"))){
@@ -16,4 +20,6 @@ rstudio_gallery <- function(){
       }
     }
   ))
+  if(is.null(img)) images
+  else images[img]
 }
